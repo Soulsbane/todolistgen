52923fc62f286e908141dbdcd8b2a5b015f1e7ad
Paul Crane <paul@acheronsoft.com>
2014-07-22 21:48:05 -0500

    Made CSV addon output to a csv file.

--- addons/csv/csv.lua
+++ addons/csv/csv.lua
@@ -1,5 +1,13 @@
 function ProcessTasks(tasks)
+	local fileName = file.getOutputPath() .. "/todo.csv"
+	local csvFile = io.open(fileName, "w+")
+
+	io.output(csvFile)
+	print("Exporting list to..." .. fileName)
+
 	for i, task in ipairs(tasks) do
-		print(task.fileName, task.lineNumber, task.tag, task.message)
+		io.write(task.fileName, ",", task.lineNumber, ",", task.tag, ",", task.message, "\n")
 	end
+
+	io.close(csvFile)
 end
