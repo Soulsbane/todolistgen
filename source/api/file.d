24720c9390632e8ccac37863aa6bcc7a68ab032b
Paul Crane <paul@acheronsoft.com>
2014-09-27 18:25:32 -0500

    Added classes FileReader and FileWriter.
    FileIO class was getting bloated and needed to be split up. These changes will hopefully address it.

--- source/api/file.d
+++ source/api/file.d
@@ -4,6 +4,52 @@ import std.file;
 import std.path;
 import std.stdio;
 
+class FileReader
+{
+public:
+	this() {}
+
+	void openFile(string fileName)
+	{
+		file_ = File(fileName, "r");
+	}
+
+	string readText(string fileName)
+	{
+		return .readText(fileName);
+	}
+private:
+	string outputDir_;
+	File file_;
+}
+
+class FileWriter
+{
+public:
+	this() {}
+
+	string openFile(string fileName)
+	{
+		string outputFile = getcwd() ~ std.path.dirSeparator ~ fileName; // FIXME: Check for custom output path.
+		file_ = File(outputFile, "w+");
+
+		return outputFile;
+	}
+
+	void write(string line)
+	{
+		file_.write(line);
+	}
+
+	void writeLine(string line)
+	{
+		file_.writeln(line);
+	}
+
+private:
+	File file_;
+}
+
 class FileAPI
 {
 public:
