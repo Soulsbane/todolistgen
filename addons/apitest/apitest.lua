39ac1348d1b9b5d4fe2dbdb4734a2053c22cb73f
Paul Crane <paul@acheronsoft.com>
2014-09-28 01:01:07 -0500

    Removed old code in apitest.lua.
    This code was used for testing sorting tasks based on filename - later used in the html and json addons.

--- addons/apitest/apitest.lua
+++ addons/apitest/apitest.lua
@@ -11,28 +11,6 @@ local function TestGetLines()
 end
 
 function ProcessTasks(tasks, size)
-	local output = {}
-
-	for i, task in ipairs(tasks) do
-		if(output[task.fileName]) then
-			table.insert(output[task.fileName], task)
-		else
-			output[task.fileName] = {}
-			table.insert(output[task.fileName], task)
-		end
-	end
-
-	for fileName, _ in pairs(output) do
-		print(fileName, "\n")
-		for outputKey, outputValue in pairs(output[fileName]) do
-			for taskTableKey, taskTableValue in pairs(outputValue) do --INFO: This loops through a task table that is stored in filename key
-				print("K3: ", taskTableKey, " V3: ", taskTableValue)
-			end
-			print("\n")
-		end
-		print("================================================")
-	end
-
 	local path = Path()
 	local fileReader = FileReader()
 
