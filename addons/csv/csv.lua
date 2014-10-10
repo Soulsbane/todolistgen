8a03a28367a4cf8d915bbbb221e0dc3ed536068f
Paul Crane <paul@acheronsoft.com>
2014-10-09 23:12:52 -0500

    Reworked csv addon to support new code conventions just introduced.

--- addons/csv/csv.lua
+++ addons/csv/csv.lua
@@ -1,15 +1,17 @@
+local fileWriter = FileWriter()
+local outputFileName = fileWriter:openFile("todo.csv")
+
 function Initialize()
+	fileWriter = FileWriter()
+	outputFileName = fileWriter:openFile("todo.csv")
+
+	print("Exporting list to..." .. outputFileName)
 end
 
 function Deinitialize()
 end
 
 function ProcessTasks(tasks, fileName)
-	local fileWriter = FileWriter()
-	local fileName = fileWriter:openFile("todo.csv")
-
-	print("Exporting list to..." .. fileName)
-
 	for i, task in ipairs(tasks) do
 		local output = task.fileName .. "," .. tostring(task.lineNumber) .. "," .. task.tag .. "," .. task.message
 		fileWriter:writeLine(output)
