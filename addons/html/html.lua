16366e2ebfcce6a760929912775edf325be8c858
Paul Crane <paul@acheronsoft.com>
2014-10-22 01:47:22 -0500

    Reworked output of exported html format.

--- addons/html/html.lua
+++ addons/html/html.lua
@@ -6,34 +6,35 @@ function Initialize()
 	local fileName = FileWriter:openFile("todo.html")
 
 	print("Exporting list to..." .. fileName)
-	FileWriter:writeLine(FileReader:readText(Path:getAddonDir() .. "/templates/default/header.html"))
+	FileWriter:write(FileReader:readText(Path:getAddonDir() .. "/templates/default/header.html"))
 end
 
 function Deinitialize()
 	local fileUtils = FileUtils()
 
-	FileWriter:writeLine(FileReader:readText(Path:getAddonDir() .. "/templates/default/footer.html"))
+	FileWriter:write(FileReader:readText(Path:getAddonDir() .. "/templates/default/footer.html"))
 	fileUtils:copyToOutputDir(Path:getAddonDir() .. "/templates/default/default.css")
 
 end
 
 local function WriteTags(...)
 	for i,v in ipairs(arg) do
-		FileWriter:writeLine(v)
+		FileWriter:write(v)
 	end
 end
 
 function ProcessTasks(tasks, fileName)
-	WriteTags("<table><caption>", fileName, "</caption>")
-	FileWriter:writeLine("<tr><th>Line Number</th><th>Message</th><th>Tag</th></tr>")
+	WriteTags("<table><caption><b>", fileName, "</b></caption>")
+	FileWriter:write('<col width="10%"><col width="10%"><col width="80%">')
+	FileWriter:write("<tr><th>Tag</th><th>Line Number</th><th>Message</th></tr>")
 
 	for i, task in ipairs(tasks) do
-		FileWriter:writeLine("<tr>")
+		FileWriter:write("<tr>")
+		WriteTags("<td>", task.tag, "</td>")
 		WriteTags("<td>", tostring(task.lineNumber), "</td>")
 		WriteTags("<td>", task.message, "</td>")
-		WriteTags("<td>", task.tag, "</td>")
-		FileWriter:writeLine("</tr>")
+		FileWriter:write("</tr>")
 	end
 
-	FileWriter:writeLine("</table>")
+	FileWriter:write("</table><br/>")
 end
