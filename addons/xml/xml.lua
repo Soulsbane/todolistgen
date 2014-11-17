66b0d312ea1b1a5ce68a5559c075d025ef534722
Paul Crane <paul@acheronsoft.com>
2014-11-17 16:18:49 -0600

    Added xml addon.

---
+++ addons/xml/xml.lua
@@ -0,0 +1,30 @@
+local FileWriter = FileWriter()
+local FileReader = FileReader()
+
+local function WriteTags(tagName, value)
+	FileWriter:writeLine("\t\t<" .. tagName .. ">" .. value .. "</" .. tagName .. ">")
+end
+
+function Initialize()
+	local fileName = FileWriter:openFile("todo.xml")
+
+	print("Exporting list to..." .. fileName)
+	FileWriter:writeLine([[<?xml version="1.0" encoding="UTF-8"?>]])
+	FileWriter:writeLine("<todo>")
+end
+
+function Deinitialize()
+	FileWriter:writeLine("</todo>")
+end
+
+function ProcessTasks(tasks, fileName)
+	FileWriter:writeLine("\t<task fileName=" .. "\"" .. fileName .. "\">")
+
+	for i, task in ipairs(tasks) do
+		WriteTags("tag", task.tag)
+		WriteTags("linenumber", task.lineNumber)
+		WriteTags("message", task.message)
+	end
+
+	FileWriter:writeLine("\t</task>")
+end
