dcd1a632b6cf430e798c4627777c453fc4fe3285
Paul Crane <paul@acheronsoft.com>
2014-12-21 18:27:37 -0600

    Added support exporting to markdown tables.

--- addons/markdown/markdown.lua
+++ addons/markdown/markdown.lua
@@ -1,11 +1,21 @@
+local FileWriter = FileWriter()
+
 function Initialize()
+	local fileName = FileWriter:openFile("todo.md")
+	print("Exporting list to..." .. fileName)
 end
 
 function Deinitialize()
 end
 
 function ProcessTasks(tasks, fileName)
+	FileWriter:writeLine("## " .. fileName)
+	FileWriter:writeLine("Tag | " .. "Line Number | " .. "Message")
+	FileWriter:writeLine("----| " .. "------------| " .. "-------")
+
 	for i, task in ipairs(tasks) do
-		print(task.fileName, task.lineNumber, task.tag, task.message)
+		FileWriter:writeLine(task.tag .. " | " .. task.lineNumber .. "|" .. task.message)
 	end
+
+	FileWriter:writeLine("\n")
 end
