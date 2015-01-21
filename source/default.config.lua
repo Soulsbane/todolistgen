5ae63dc20e925048d00f89611bc38a965e04687b
Paul Crane <paul@acheronsoft.com>
2015-01-20 21:07:35 -0600

    Updated patterns in default.config.lua

--- source/default.config.lua
+++ source/default.config.lua
@@ -1,5 +1,6 @@
 TodoTaskPatterns = {
 	["([A-Z]+):(.*)"] = false,
 	["\\W+([a-zA-Z]+):\\s+(.*)"] = false,
-	["\\W+(INFO|NOTE|FIXME|TODO):\\s+(.*)"] = true,
+	["\\W+(INFO|NOTE|FIXME|TODO):\\s+(.*)"] = false,
+	["[;'#-*@/]*\\s*(INFO|NOTE|FIXME|TODO|XXX):?\\s*(.*)"] = true,
 }
