0498db2c97803264fd56dfbc67531fe19fd46f22
Paul Crane <paul@acheronsoft.com>
2015-05-05 19:10:15 -0500

    Made fileutils API functions use buildNormalizedPath.

--- source/api/fileutils.d
+++ source/api/fileutils.d
@@ -6,8 +6,6 @@ import std.path;
 import api.path;
 import config;
 
-alias sep = std.path.dirSeparator;
-
 void copyFileTo(string from, string to) @trusted
 {
 	std.file.copy(from, to, PreserveAttributes.yes);
@@ -15,12 +13,12 @@ void copyFileTo(string from, string to) @trusted
 
 void copyToOutputDir(string fileName) @trusted
 {
-	std.file.copy(fileName, getcwd() ~ sep ~ baseName(fileName), PreserveAttributes.yes);
+	std.file.copy(fileName, buildNormalizedPath(getcwd(), baseName(fileName)), PreserveAttributes.yes);
 }
 
 void removeFileFromAddonDir(string fileName) @trusted
 {
-	string file = getAddonDir() ~ sep ~ fileName;
+	string file = buildNormalizedPath(getAddonDir(), fileName);
 
 	if(exists(file))
 	{
@@ -30,7 +28,7 @@ void removeFileFromAddonDir(string fileName) @trusted
 
 void removeFileFromOutputDir(string fileName) @trusted
 {
-	string file = getOutputDir() ~ sep ~ fileName;
+	string file = buildNormalizedPath(getOutputDir(), fileName);
 
 	if(exists(file))
 	{
