6bb185ac4aa662e1542877598c8299cc170fc925
Paul Crane <paul@acheronsoft.com>
2015-06-11 02:41:04 -0500

    Tweaked how addonextractor works.

--- source/addonextractor.d
+++ source/addonextractor.d
@@ -1,48 +1,60 @@
 import std.stdio;
 import std.path;
-import std.file;
+import std.file: exists, mkdirRecurse;
+import std.algorithm;
+import std.array;
+import std.typetuple;
 
 import api.path;
 
+enum fileNames =
+[
+	"apitest/apitest.lua",
+	"apitest/data.txt",
+	"csv/csv.lua",
+	"html/html.lua",
+	"html/modules/etlua.lua",
+	"html/templates/default/content.html",
+	"html/templates/default/footer.html",
+	"html/templates/default/header.html",
+	"html/templates/default/default.css",
+	"html/templates/default/tasks.elt",
+	"json/json.lua",
+	"markdown/markdown.lua",
+	"stdout/stdout.lua",
+	"xml/xml.lua"
+];
+
+// Really cool trick learned from reggae* source code. D really is awesome!
+// * https://github.com/atilaneves/reggae/blob/master/src/reggae/reggae.d
+private string filesTupleString() @safe pure nothrow
+{
+	return "TypeTuple!(" ~ fileNames.map!(a => `"` ~ a ~ `"`).join(",") ~ ")";
+}
+
+template FileNames()
+{
+    mixin("alias FileNames = " ~ filesTupleString ~ ";");
+}
+
 void extractFiles()
 {
-	immutable string[string] data =
-	[
-		"apitest/apitest.lua" : import("apitest/apitest.lua"),
-		"apitest/data.txt" : import("apitest/data.txt"),
-
-		"csv/csv.lua" : import("csv/csv.lua"),
-		"html/html.lua" : import("html/html.lua"),
-
-		"html/modules/etlua.lua" : import("html/modules/etlua.lua"),
-		"html/templates/default/content.html" : import("html/templates/default/content.html"),
-		"html/templates/default/footer.html" : import("html/templates/default/footer.html"),
-		"html/templates/default/header.html" : import("html/templates/default/header.html"),
-		"html/templates/default/default.css" : import("html/templates/default/default.css"),
-		"html/templates/default/tasks.elt" : import("html/templates/default/tasks.elt"),
-
-		"json/json.lua" : import("json/json.lua"),
-		"markdown/markdown.lua" : import("markdown/markdown.lua"),
-		"stdout/stdout.lua" : import("stdout/stdout.lua"),
-		"xml/xml.lua" : import("xml/xml.lua")
-	];
-
 	debug
 	{}
 	else
 	{
-		foreach(dirString, importString; data)
+		foreach(name; FileNames!())
 		{
-			string pathStr = dirName(buildNormalizedPath(getBaseAddonDir(), dirString));
-			string fileName = buildNormalizedPath(getBaseAddonDir(), dirString);
+			string filePath = dirName(buildNormalizedPath(getBaseAddonDir(), name));
+			string pathWithFileName = buildNormalizedPath(getBaseAddonDir(), name);
 
-			if(!exists(pathStr))
+			if(!exists(filePath))
 			{
-				mkdirRecurse(pathStr);
+				mkdirRecurse(filePath);
 			}
 
-			auto file = File(fileName, "w");
-			file.write(importString);
+			auto fileHandle = File(pathWithFileName, "w");
+			fileHandle.write(import(name));
 		}
 	}
 }
