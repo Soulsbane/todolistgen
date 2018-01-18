3b272d3c629518f06c76e24911c774f4fc8d7733
Paul Crane <paul@acheronsoft.com>
2018-01-18 02:09:43 -0600

    Add module todofilereader.d

---
+++ source/todofilereader.d
@@ -0,0 +1,77 @@
+module todofilereader;
+
+import std.stdio;
+import std.encoding;
+import std.string;
+import std.regex;
+import std.conv;
+import std.file;
+
+alias string[string] TaskValues;
+
+class TodoFileReader
+{
+public:
+	this()
+	{
+		pattern_ = getConfigPattern();
+	}
+
+	TaskValues[] readFile(const string fileName) @trusted
+	{
+		TaskValues[] tasks;
+
+		foreach(ulong i, string line; File(fileName, "r").lines)
+		{
+			if(line.isValid) // INFO: Make sure the line is actually text.
+			{
+				auto values = createTask(fileName, i + 1, line);
+				if(values.length > 0)
+				{
+					tasks ~= values;
+				}
+			}
+		}
+		return tasks;
+	}
+
+private:
+	TaskValues createTask(const string curFileName, const ulong lineNum, const string line) @safe
+	{
+		auto re = regex(pattern_, "g");
+		auto match = matchFirst(line, re);
+		auto nc = re.namedCaptures;
+		TaskValues values;
+
+		if(match)
+		{
+			values["fileName"] = curFileName;
+			values["lineNumber"] = to!string(lineNum);
+
+			for(int i = 0; i <= (nc.length - 1); i++)
+			{
+				values[to!string(nc[i])] = to!string(match[nc[i]]);
+			}
+		}
+		return values;
+	}
+
+	string getConfigPattern() @trusted const
+	{
+		auto config = new LuaConfig;
+		auto variable = config.getTable("TodoTaskPatterns");
+		string found;
+
+		foreach(string key, bool value; variable)
+		{
+			if(value == true)
+			{
+				found = key;
+			}
+		}
+		return found;
+	}
+
+private:
+	string pattern_;
+}
