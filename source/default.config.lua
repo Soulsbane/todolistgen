864a8f249a32b9c8ea78ed2837853ac2505ac1af
Paul Crane <paul@acheronsoft.com>
2018-01-18 22:44:23 -0600

    Add default config.lua file.

---
+++ source/default.config.lua
@@ -0,0 +1,12 @@
+TodoTaskPatterns = {
+	["(?P<tag>[A-Z]+):(?P<message>.*)"] = false,
+	["\\W+(?P<tag>[a-zA-Z]+):\\s+(?P<message>.*)"] = false,
+	["\\W+(?P<tag>INFO|NOTE|FIXME|TODO):\\s+(?P<message>.*)"] = false,
+	["[;'#-*@/]*\\s*(?P<tag>INFO|NOTE|FIXME|TODO|XXX):?\\s*(?P<message>.*)"] = true,
+}
+
+AppConfigVars = {
+	DeleteAllTodoFilesAtStart = true,
+	DefaultTodoFileName = "todo",
+}
+
