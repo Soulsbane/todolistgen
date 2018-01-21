80781de2b5f9dfbed75ad12a29be887825735a98
Paul Crane <paul@acheronsoft.com>
2018-01-20 22:35:07 -0600

    Add module api.path.

---
+++ source/api/path.d
@@ -0,0 +1,38 @@
+module api.path;
+
+import std.file : exists, getcwd, thisExePath;
+import std.path : dirName, buildNormalizedPath;
+
+import luaaddon.addonpaths;
+
+class ApplicationPaths : AddonPaths
+{
+
+	this(const string addonName, const string applicationName, const string organizationName = string.init)
+	{
+		super(addonName, applicationName, organizationName);
+	}
+	// TODO: Perhaps support these methods?
+	/*string getOutputDir()
+	{
+		return buildNormalizedPath(getcwd(), getValueFor("ProjectName"));
+	}
+
+	bool createDirInOutputDir(const(char)[][] params...)
+	{
+		immutable string path = buildNormalizedPath(params);
+		return ensurePathExists(buildNormalizedPath(getOutputDir(), path));
+	}
+
+	bool removeDirFromOutputDir(const string dir)
+	{
+		return removePathIfExists(getOutputDir(), dir);
+	}
+
+	bool outputDirExists(const string dir)
+	{
+		string file = buildNormalizedPath(getOutputDir(), dir);
+		return file.exists;
+	}
+	*/
+}
