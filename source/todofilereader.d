3b237a7e5f4eff40feb020ac4693c98d63fc3879
Paul Crane <paul@acheronsoft.com>
2018-01-22 18:40:03 -0600

    Add note about multiple regex pattern matching.

--- source/todofilereader.d
+++ source/todofilereader.d
@@ -67,7 +67,7 @@ private:
 		{
 			if(value == true)
 			{
-				found = key;
+				found = key; // NOTE: This isn't a bug. Support for multiple patterns to match will be added later.
 			}
 		}
 		return found;
