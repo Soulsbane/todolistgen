d5422f270156ad5d4e62040aa9f40e049a038420
Paul Crane <paul@acheronsoft.com>
2018-01-27 23:59:54 -0600

    Add load method to Config class.

--- source/config.d
+++ source/config.d
@@ -1,12 +1,32 @@
 module config;
 
+import std.path;
+import std.file;
+import std.stdio;
+
 import luaaddon.luaconfig;
+import api.path;
 
 enum ORGANIZATIONNAME = "Raijinsoft";
 enum APPLICATIONNAME = "todolistgen";
 
 class Config : LuaConfig
 {
+	void load(const string fileName, const string addonName = string.init) @trusted
+	{
+		auto paths = new ApplicationPaths(addonName);
+		immutable string configPath = paths.getConfigFilesDir();
+		immutable string configFile = buildNormalizedPath(configPath, fileName);
 
+		if(!configFile.exists)
+		{
+			immutable string configText = import("default.config.lua");
+			auto file = File(configFile, "w+");
+
+			file.write(configText);
+		}
+
+		loadFile(configFile);
+	}
 }
 
