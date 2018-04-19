05a6dd06744acb8fcd6714b01af6200543e9d7cc
Paul Crane <paul@acheronsoft.com>
2018-04-19 01:30:10 -0500

    Clean up json generator.

--- generators/json/json.lua
+++ generators/json/json.lua
@@ -11,27 +11,26 @@ function OnDestroy()
 	print("Exporting list to " .. TodoFileName)
 end
 
-function OnProcessTasks(tasks, fileName, lastFile)
-		FileHandle:write(string.format("\t%q: [\n" , fileName))
+function OnProcessTasks(tasks, fileName, isLastFile)
+	FileHandle:write(string.format("\t%q: [\n", fileName))
 
-		for i, task in pairs(tasks) do
+	for i, task in pairs(tasks) do
+		FileHandle:write("\t{\n")
 
-			FileHandle:write("\t{\n")
+		FileHandle:write(string.format("\t\t%q: %d,\n", "lineNumber", task.lineNumber))
+		FileHandle:write(string.format("\t\t%q: %q,\n", "message", task.message))
+		FileHandle:write(string.format("\t\t%q: %q\n", "tag", task.tag))
 
-			FileHandle:write(string.format("\t\t%q: %d,\n", "lineNumber", task.lineNumber))
-			FileHandle:write(string.format("\t\t%q: %q,\n", "message", task.message))
-			FileHandle:write(string.format("\t\t%q: %q\n", "tag", task.tag))
-
-			if(i == #tasks) then
-				FileHandle:write("\t}\n")
-			else
-				FileHandle:write("\t},\n")
-			end
-		end
-		if lastFile then
-			FileHandle:write("\t]\n")
+		if i == #tasks then
+			FileHandle:write("\t}\n")
 		else
-			FileHandle:write("\t],\n")
+			FileHandle:write("\t},\n")
 		end
-end
+	end
 
+	if isLastFile then
+		FileHandle:write("\t]\n")
+	else
+		FileHandle:write("\t],\n")
+	end
+end
