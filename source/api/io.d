bd39e772e4022c2a89f18fe81b6d93e98dc1951f
Paul Crane <paul@acheronsoft.com>
2018-07-07 23:14:49 -0500

    Add documentation for readText and getLines API's.

--- source/api/io.d
+++ source/api/io.d
@@ -2,12 +2,24 @@ module api.io;
 
 import std.string;
 
+/**
+	Reads the file into a string.
+
+	Params:
+		fileName = Name of the file to read.
+*/
 string readText(const string fileName)
 {
 	import std.file : readText;
 	return readText(fileName);
 }
 
+/**
+	Reads each line of the file into an array of strings.
+
+	Params:
+		fileName = Name of the file to read.
+*/
 string[] getLines(string fileName) @safe
 {
 	import std.file : readText;
